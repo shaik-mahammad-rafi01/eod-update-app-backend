003fe5cb0f0427ddaf939fefc183af64b81fd03f
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-12-01 16:35:32 +0530

    Update static data with id

--- src/Data/EodUpdatesData.ts
+++ src/Data/EodUpdatesData.ts
@@ -3,12 +3,14 @@ export const EodUpdatesData : EodType[] = [
     {
         title: "2025-12-1",
         description: "EOD Updates",
-        blockers: "configuration issues"
+        blockers: "configuration issues",
+        id: "1"
     },
     {
         title: "2025-12-01",
         description: "backend",
-        blockers: "netwrks issue"
+        blockers: "netwrks issue",
+        id: "2"
     }
 
 ]
