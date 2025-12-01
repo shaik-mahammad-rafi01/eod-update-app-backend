2410a28cde905153ace19aeb7a2d6c6c9d8ffd14
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-12-01 16:11:03 +0530

    Add static EodUpdates data

---
+++ src/Data/EodUpdatesData.ts
@@ -0,0 +1,14 @@
+import type { EodType } from "../types/EodType.js"
+export const EodUpdatesData : EodType[] = [
+    {
+        title: "2025-12-1",
+        description: "EOD Updates",
+        blockers: "configuration issues"
+    },
+    {
+        title: "2025-12-01",
+        description: "backend",
+        blockers: "netwrks issue"
+    }
+
+]
